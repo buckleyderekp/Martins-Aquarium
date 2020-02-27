13399a599405157717135aa65fcc6d63e8445134
buckleyderekp <buckleyderekp@gmail.com>
2020-02-27 10:09:11 -0600

    finished fish object and function

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,48 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        species: "Orange Clownfish",
+        location: "Carribean",
+        length: "3 inches",
+        food: ["algae", "crustaceans"]
+    },
+    {
+        name: "Betty",
+        species: "Yellow Tang",
+        location: "Carribean",
+        length: "7.9 inches",
+        food: ["benthic turf algae"]
+    },
+    {
+        name: "Fred",
+        species: "Lionfish",
+        location: "Carribean",
+        length: "6 inches",
+        food: ["small fish", "invertabrates", "mollusks"]
+    },
+    {
+        name: "Gertrude",
+        species: "Discus Fish",
+        location: "Carribean",
+        length: "9 inches",
+        food: ["zooplankton", "inscects"]
+    },
+    {
+        name: "Bob",
+        species: "Blob Fish",
+        location: "Atlantic Ocean",
+        length: "12 inches",
+        food: ["sea urchins", "mollusks"]
+    },
+    {
+        name: "Clementine",
+        species: "Angelfish",
+        location: "Pacific Ocean",
+        length: "9 inches",
+        food: ["algae", "jellyfish", "plankton"]
+    },
+
+]
+export const useFish = () => {
+    return fishCollection
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import initializeDetailButtonEvents from './dialogs.js'
+import { useFish } from './FishDataProvider.js'
 
+const arrayOfTheFishObjects = useFish()
 initializeDetailButtonEvents()
+
+for (const fishObject of arrayOfTheFishObjects) {
+    console.log(fishObject)
+}
